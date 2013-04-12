312f6b3c67e07efd37844b53ab55717417b77b2e
ysano <ysano@ysnet.org>
2013-04-12 16:50:07 +0900

    w32 ime setting

--- .emacs.d/init-w32.el
+++ .emacs.d/init-w32.el
@@ -11,6 +11,31 @@
 (setq default-process-coding-system '(cp932 . cp932))
 
 ;; ------------------------------------------------------------------------
+;; @ ime
+
+;; 標準IMEの設定
+(setq default-input-method "W32-IME")
+
+;; IME状態のモードライン表示
+(setq-default w32-ime-mode-line-state-indicator "[Aa]")
+(setq w32-ime-mode-line-state-indicator-list '("[Aa]" "[あ]" "[Aa]"))
+
+;; IMEの初期化
+(w32-ime-initialize)
+
+;; IME OFF時の初期カーソルカラー
+;; (set-cursor-color "red")
+
+;; IME ON/OFF時のカーソルカラー
+;; (add-hook 'input-method-activate-hook
+;;           (lambda() (set-cursor-color "green")))
+;; (add-hook 'input-method-inactivate-hook
+;;           (lambda() (set-cursor-color "red")))
+
+;; バッファ切り替え時にIME状態を引き継ぐ
+(setq w32-ime-buffer-switch-p nil)
+
+;; ------------------------------------------------------------------------
 ;; @ font
 
 ;; 標準フォントの設定
