6c3a5ba7ded98fb0e6376fc2696bfc0794062162
ysano <ysano@ysnet.org>
2014-10-22 10:03:07 +0900

    js3 options

--- .emacs.d/init-elpa.el
+++ .emacs.d/init-elpa.el
@@ -209,6 +209,9 @@
 ;; js3-mode
 (autoload 'js3-mode "js3" nil t nil)
 (add-to-list 'auto-mode-alist '("\\.js$" . js3-mode))
+(setq js3-auto-indent-p t         ; it's nice for commas to right themselves.
+      js3-enter-indents-newline t ; don't need to push tab before typing
+      js3-indent-on-enter-key t)   ; fix indenting before moving on
 
 ;; jade-mode
 (autoload 'jade-mode "jade-mode" nil t nil)
