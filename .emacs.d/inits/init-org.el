3f017c6fe416d712caa475a928ffee6ed45ed1aa
ysano <ysano@ysnet.org>
2018-06-25 12:22:42 +0900

    reveal version 3.6.0

--- .emacs.d/inits/init-org.el
+++ .emacs.d/inits/init-org.el
@@ -299,7 +299,7 @@
 
 ;; ox-reveal
 (require 'ox-reveal)
-(setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
+(setq org-reveal-root "https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0/")
 
 ;;------------------------------------------------------------
 ;; Shortcut
