f89100b32264aa32b0c20a17f2e45fd83ec2f5b2
ysano <ysano@ysnet.org>
2019-06-15 13:12:22 +0900

    feat(git-gutter): add keybind

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -593,6 +593,12 @@
   :diminish
   :custom
   (git-gutter:update-interval 2)
+  :bind (("C-x v =" . 'git-gutter:popup-hunk)
+         ("C-x p" . 'git-gutter:previous-hunk)
+         ("C-x n" . 'git-gutter:next-hunk)
+         ("C-x v s" . 'git-gutter:stage-hunk)
+         ("C-x v r" . 'git-gutter:revert-hunk)
+         ("C-x v SPC" . #'git-gutter:mark-hunk))
   :config
   (global-git-gutter-mode t))
 
