5d5574e45ca9292b055236ea17dead77a379694e
ysano <ysano@ysnet.org>
2021-02-09 10:50:07 +0900

    feat(multiple-cursors)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -766,6 +766,14 @@
   :config
   (editorconfig-mode 1))
 
+;; multiple-cursors
+(use-package multiple-cursors :ensure t
+  :bind (("C-S-c C-S-c" . mc/edit-lines)
+         ("C->" . mc/mark-next-like-this)
+         ("C-<" . mc/mark-previous-like-this)
+         ("C-c C-<" . mc/mark-all-like-this)
+         ))
+
 ;;--------------------------------
 ;; auto-complete
 ;;--------------------------------
