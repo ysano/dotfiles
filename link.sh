52d1a2f9aca795340dca894d26d89d05988cdae4
ysano <ysano@ysnet.org>
2022-09-19 12:46:16 +0900

    fix(link): msys

--- link.sh
+++ link.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/zsh
 files=(.zshrc .gitconfig .tmux.conf .tmux-linux.conf .tmux-freebsd.conf .aspell.conf .xinitrc .Xresources)
 dirs=(.zsh .emacs.d)
-dotfiles=./dotfiles
+dotfiles=dotfiles
 
-for f in $files; do
-    if [[ -f $HOME/$f ]] && mv $HOME/$f $HOME/$f.orig
-    ln -s $dotfiles/$f $HOME/$f
-done
+case "${OSTYPE}" in
+    msys)
+        for f in $files; do
+            cmd <<< "mklink ${f} ${dotfiles}\\${f}"
+        done
+        for d in $dirs; do
+            cmd <<< "mklink /D ${d} ${dotfiles}\\${d}"
+        done
+        ;;
+    *)
+        for f in $files; do
+            if [[ -f $HOME/$f ]] && mv $HOME/$f $HOME/$f.orig
+               ln -s $dotfiles/$f $HOME/$f
+        done
+        for d in $dirs; do
+            if [[ -d $HOME/$d ]] && mv $HOME/$d $HOME/$d.orig
+               ln -s $dotfiles/$d $HOME/$d
+        done
+        ;;
+esac
 
-for d in $dirs; do
-    if [[ -d $HOME/$d ]] && mv $HOME/$d $HOME/$d.orig
-    ln -s $dotfiles/$d $HOME/$d
-done
