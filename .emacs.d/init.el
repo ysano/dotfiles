ad015e2805788504cca5409a1cfd0cad5b09b9a4
ysano <ysano@ysnet.org>
2024-08-13 03:26:23 +0900

    feat: ellama

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1056,6 +1056,48 @@ _m_agit  _b_lame  _d_ispatch  _t_imemachine  |  hunk: _p_revious  _n_ext  _s_tag
 ;; you can utilize :map :hook and :config to customize copilot
 
 ;;--------------------------------
+;; ellama
+;;--------------------------------
+(use-package ellama :ensure t
+  :config
+  (require 'llm-ollama)
+  :bind-keymap
+  ("C-c e" . ellama-keymap)
+  :custom
+  (ellama-language "Japanese")
+  (ellama-provider
+   (make-llm-ollama
+    ;; this model should be pulled to use it
+    ;; value should be the same as you print in terminal during pull
+    :chat-model "llama3:8b-instruct-q8_0"
+    :embedding-model "nomic-embed-text"
+    :default-chat-non-standard-params '(("num_ctx" . 8192))))
+  ;; Predefined llm providers for interactive switching.
+  ;; You shouldn't add ollama providers here - it can be selected interactively
+  ;; without it. It is just example.
+  (ellama-providers
+   '(("zephyr" . (make-llm-ollama
+                  :chat-model "zephyr:7b-beta-q6_K"
+                  :embedding-model "zephyr:7b-beta-q6_K"))
+     ("mistral" . (make-llm-ollama
+                   :chat-model "mistral:7b-instruct-v0.2-q6_K"
+                   :embedding-model "mistral:7b-instruct-v0.2-q6_K"))
+     ("mixtral" . (make-llm-ollama
+                   :chat-model "mixtral:8x7b-instruct-v0.1-q3_K_M-4k"
+                   :embedding-model "mixtral:8x7b-instruct-v0.1-q3_K_M-4k"))))
+  ;; Naming new sessions with llm
+  (ellama-naming-provider
+   (make-llm-ollama
+    :chat-model "llama3:8b-instruct-q8_0"
+    :embedding-model "nomic-embed-text"
+    :default-chat-non-standard-params '(("stop" . ("\n")))))
+  (ellama-naming-scheme 'ellama-generate-name-by-llm)
+  ;; Translation llm provider
+  (ellama-translation-provider (make-llm-ollama
+                                :chat-model "phi3:14b-medium-128k-instruct-q6_K"
+                                :embedding-model "nomic-embed-text")))
+
+;;--------------------------------
 ;; org-mode
 ;;--------------------------------
 (use-package org :ensure t :pin gnu
